94c4ad2910ff2b1c1a82a91f65aa3a155053b549
Gorghiu Ciprian <100276820+CiprianGorghiu@users.noreply.github.com>
2023-03-21 10:37:54 +0200

    Add files via upload

---
+++ process_form.php
@@ -0,0 +1,32 @@
+<?php
+if (isset($_POST['submit'])) {
+  // preluam datele din formular
+  $username = $_POST['username'];
+  $password = $_POST['password'];
+
+  // conectare la baza de date
+  $servername = "localhost";
+  $username_db = "id19076202_test";
+  $password_db = "@d9+K79{glwGePKH";
+  $database = "id19076202_users";
+
+  $conn = mysqli_connect($servername, $username_db, $password_db, $database);
+
+  // verificare conexiune
+  if (!$conn) {
+    die("Conexiunea a esuat: " . mysqli_connect_error());
+  }
+
+  // pregatirea si executarea interogarii SQL
+  $sql = "INSERT INTO users (username, password) VALUES ('$username', '$password')";
+  if (mysqli_query($conn, $sql)) {
+    header("Location: error_page.html");
+    exit;
+  } else {
+    echo "A aparut o eroare: " . mysqli_error($conn);
+  }
+
+  // inchiderea conexiunii cu baza de date
+  mysqli_close($conn);
+}
+?>
